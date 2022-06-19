e138d3971151160873fadee162f27f6c5588ce97
alex reinoso <alex.reinoso@bayteq.com>
2022-06-19 18:06:16 -0500

    adding DAOS to give them a try

--- src/com/stackbuilders/app/Predictor.java
+++ src/com/stackbuilders/app/Predictor.java
@@ -1,6 +1,10 @@
 package com.stackbuilders.app;
 
 
+import com.stackbuilders.dao.DAOFactory;
+import com.stackbuilders.dao.PicoPlacaDAO;
+import com.stackbuilders.dao.QueryDAO;
+import com.stackbuilders.dao.ScheduleDAO;
 import com.stackbuilders.service.PicoPlacaService;
 import com.stackbuilders.service.ScheduleService;
 
@@ -10,7 +14,9 @@ public class Predictor {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-	
+		PicoPlacaDAO picoPlacaDAO = DAOFactory.getFactory().getPicoPlacaDAO();
+		ScheduleDAO scheduleDAO = DAOFactory.getFactory().getScheduleDAO();
+		QueryDAO queryDAO = DAOFactory.getFactory().getQueryDAO();
 			
 		
 	}
