f00152f008525704c498768d3a6d466224345cd8
alex reinoso <alex.reinoso@bayteq.com>
2022-06-19 14:59:39 -0500

    class model for the schedule
    This class help in builder the set of rules for pico y placa

--- src/com/stackbuilders/entity/Schedule.java
+++ src/com/stackbuilders/entity/Schedule.java
@@ -2,9 +2,51 @@ package com.stackbuilders.entity;
 
 public class Schedule {
 	
+	private int id;
 	private String day;
-	private String start;
-	private String end;
-	private String[] endNumbers;
+	private String startHour;
+	private String endHour;
+	
+	public Schedule(int id, String day, String startHour, String endHour) {
+		super();
+		this.id = id;
+		this.day = day;
+		this.startHour = startHour;
+		this.endHour = endHour;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getStartHour() {
+		return startHour;
+	}
+
+	public void setStartHour(String startHour) {
+		this.startHour = startHour;
+	}
+
+	public String getEndHour() {
+		return endHour;
+	}
+
+	public void setEndHour(String endHour) {
+		this.endHour = endHour;
+	}
+	
+	
+	
 }
