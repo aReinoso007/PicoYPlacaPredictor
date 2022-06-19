f4046a3fa40e6254f1f8f0cbdb407153c24d4223
alex reinoso <alex.reinoso@bayteq.com>
2022-06-19 15:18:04 -0500

    Enums for building the pico y placa schedule

---
+++ src/com/stackbuilders/utils/DayEnums.java
@@ -0,0 +1,36 @@
+package com.stackbuilders.utils;
+
+import java.util.Calendar;
+
+public enum DayEnums {
+	
+	MON(Calendar.MONDAY, "Monday"),
+	TUE(Calendar.TUESDAY, "Tuesday"),
+	WED(Calendar.WEDNESDAY, "Wednesday"),
+	THU(Calendar.THURSDAY, "Thursday"),
+	FRI(Calendar.FRIDAY, "Friday");
+	
+	private int dayNumber;
+	private String dayName;
+	
+	private DayEnums(int dayNumber, String dayName) {
+		this.dayNumber = dayNumber;
+		this.dayName = dayName;
+	}
+	
+	public int getDayNumber() {
+		return dayNumber;
+	}
+	public void setDayNumber(int dayNumber) {
+		this.dayNumber = dayNumber;
+	}
+	public String getDayName() {
+		return dayName;
+	}
+	public void setDayName(String dayName) {
+		this.dayName = dayName;
+	}
+	
+	
+
+}
