ee24c98c2ffba853e99ebe8e0459fb7c3da07c69
alex reinoso <34074669+aReinoso007@users.noreply.github.com>
2022-06-19 22:03:22 -0500

    Junit testing for schedule service

--- src/com/stackbuilders/test/ScheduleTest.java
+++ src/com/stackbuilders/test/ScheduleTest.java
@@ -1,6 +1,32 @@
 package com.stackbuilders.test;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.stackbuilders.dao.DAOFactory;
+import com.stackbuilders.dao.ScheduleDAO;
+import com.stackbuilders.entity.PicoPlaca;
+import com.stackbuilders.entity.Schedule;
 
 public class ScheduleTest {
+	
+	ScheduleDAO scheduleDAO = DAOFactory.getFactory().getScheduleDAO();
+	
+	@Test
+	public void testGeneratedMorningHourRange() {
+		Schedule expected = new Schedule("07:00", "09:30");
+		Schedule actual = scheduleDAO.generateMorningHourRange();
+		Assertions.assertEquals(expected, actual);
+		
+	}
+	
+	@Test
+	public void testGeneratedNoonHourRange() {
+		Schedule expected = new Schedule("16:00", "19:30");
+		Schedule actual = scheduleDAO.generateNoonHourRange();
+		Assertions.assertEquals(expected, actual);
+	}
 
 }
